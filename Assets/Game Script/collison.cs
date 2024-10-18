479ae9d27d779b707c15c80e4969c450bd2ea2a3
Sujal1s <pachpandesujal555@gmail.com>
2024-10-18 23:36:45 +0530

    Tower Destroye logic , score script  , assets and script
    Tower Destroyed logic added , score script created , organized more assets and script

---
+++ Assets/Game Script/collison.cs
@@ -0,0 +1,15 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlaneCollision : MonoBehaviour
+    
+{
+    public GameObject Plane;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.gameObject)
+        {
+            Destroy(collision.gameObject); // Destroy the tower
+        }
+    }
+}
